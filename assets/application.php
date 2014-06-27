a01a2a6fd0d81bae7cc5177004b61fc424087a54
jonyen <jonyen@gmail.com>
2014-06-27 16:22:59 -0700

    file cleanup

---
+++ assets/application.php
@@ -0,0 +1,68 @@
+<?php 
+$passages = explode(",", $_GET['passages']);
+$translations = ["ESV", "CNVT", "NVI", "SZ", "BPT"];
+
+$biblegateway_url="https://www.biblegateway.com/passage/";
+
+foreach ($translations as $translation) { 
+        echo "<div class='block'>";
+        echo "<hr />";
+	echo "<span class='simptip-position-right simptip-multiline simptip-smooth simptip-info simptip-fade' data-tooltip=\"Press Ctrl+C or Cmd+C to copy to clipboard after clicking on 'Select all'\"><a href='javascript:void(0);' id='$translation-select' onclick='selectText(\"$translation\")' class='button white noprint'>Select text</a></span>";
+	echo "<div class='info noprint'>Show verse references <input type='checkbox' onchange='javascript:toggleVerses(\"$translation\")' checked /></div>";
+        echo "<div id='$translation' class='translation'>";
+	foreach ($passages as $passage) {
+		$passage = rawurlencode($passage);
+		$curl_handle = curl_init();
+
+		curl_setopt($curl_handle, CURLOPT_URL, $biblegateway_url);
+		curl_setopt($curl_handle, CURLOPT_POSTFIELDS, "search=$passage&version=$translation");
+		curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+
+		$result = curl_exec($curl_handle);
+
+		echo "<div class='passages'>";
+		
+		// Since CNVT has a glitch and shows simplified characters for the verse reference, we grab the traditional characters from CUVMPT instead
+		if ($translation == "CNVT") {
+			$traditional_title = curl_init();
+			curl_setopt($traditional_title, CURLOPT_URL, $biblegateway_url);
+			curl_setopt($traditional_title, CURLOPT_POSTFIELDS, "search=$passage&version=CUVMPT");
+			curl_setopt($traditional_title, CURLOPT_RETURNTRANSFER, 1);
+			
+			$traditional = curl_exec($traditional_title);
+			preg_match("/<h1 class=\"bcv\">(.+?)<\/h1>/", $traditional, $matches); 
+		} else {
+			preg_match("/<h1 class=\"bcv\">(.+?)<\/h1>/", $result, $matches);
+		}
+
+		echo  "<div style='text-align: center'><span class='fleuron'>d</span>  $matches[1]  <span class='fleuron'>c</span></div>"; 
+		preg_match("/(<div class=\"version-$translation result-text-style-normal text-html \">.+?)<div class=\"publisher-info-bottom\">/s", $result, $matches);
+		$result = $matches[1]; 
+
+		// remove passage display 
+		$result = preg_replace("/<div class=\"passage-display\">.+?<\/div>/s", "", $result); 
+
+	        // change chapter number to verse number 1
+                $result = preg_replace("/<span class=\"chapternum\">.+?<\/span>/", "<sup class=\"versenum\">1&nbsp;</sup>", $result);
+
+		// remove titles
+		$result = preg_replace("/<h3>.+?<\/h3>/s", "", $result);
+
+		// remove crossrefs and footnotes 
+		$result = preg_replace("/<sup data-(cr|fn)=['\"].+?['\"] class=['\"](crossref|footnote)['\"].+?>.?<\/sup>/s", "", $result); 
+		$result = preg_replace("/<div class=\"(crossrefs hidden|footnotes)\">.+?<\/div>/s", "", $result);  
+//		$result = preg_replace("/<h4>(Cross references|Footnotes):<\/h4>/", "", $result); 
+/*		$result = preg_replace("/<ol .+?>.+?<\/ol>/s", "", $result);   */
+		$result = preg_replace("/(<sup class=\"versenum\">)/", "&#8203;$1", $result);  // adds zero-width space so that the verse num will break from previous word
+//		$result = preg_replace("/<span.+?class=\"text.+?\">/", "", $result);
+//		$result = preg_replace("/<\/span>/", "", $result);
+		$result = preg_replace("/[\n\r\f]+/m","", $result);
+		echo $result;
+		echo "</div>";
+
+		curl_close($curl_handle);
+	}
+        echo "</div>"; 
+        echo "</div>"; 
+} 
+?>
---
+++ javascripts/functions.js
@@ -0,0 +1,184 @@
+var bcv = new bcv_parser; 
+
+function getJSON(seq) {
+  var json = {};
+  passages = bcv.parse(seq).osis().split(',');
+  passages.forEach(function(passage) {
+    json[passage] = getChapters(passage);
+  });
+  return json;
+}
+
+function getChapters(ref) {
+  passage = bcv.parse(ref);
+    switch(passage.entities[0].type) {
+      case 'bcv':
+      case 'bc':
+        return [passage.entities[0].passages[0].start.b + " " + passage.entities[0].passages[0].start.c];
+      case 'b':
+      case 'range':
+        chapters = [];
+        start = passage.entities[0].passages[0].start;
+        end = passage.entities[0].passages[0].end;
+        book = start.b;
+        chapter = start.c || 1;
+        while (true) {
+	  // force a "1" verse so that it gets detected in special cases such as Philemon or 2 John
+          if (bcv.parse(book + " " + chapter + ":1").osis() == "" || chapter > end.c) {  
+            return chapters;
+          }
+        chapters.push(book + " " + chapter);
+        chapter++;
+        }
+      case 'sequence':
+        return passage.osis()
+		      .split(",")
+		      .map(function(p) { return getChapters(p) });
+      default:
+        return [];
+    }
+}
+
+function getVerses(str) {
+  passage = bcv.parse(str);
+  switch (passage.entities[0].type) {
+    case 'bcv':
+      return [passage.s];
+    case 'bc':
+      start = passage.entities[0].passages[0].start;
+      book = start.b;
+      chapter = start.c;
+      verse = 1;
+      var verses = [];
+      while (true) {
+        ref = book + " " + chapter + ":" + verse;
+        curParse = bcv.parse(ref);
+        if (curParse.osis() == "") {
+          return verses.map(function(v) { return bcv.parse(v).osis(); });
+	}
+        verses.push(ref);
+        verse++;
+      }
+    case 'b':
+      start = passage.entities[0].passages[0].start;
+      book = start.b;
+      chapter = 1;
+      verse = 1;
+      var verses = [];
+      while (true) {
+        ref = book + " " + chapter + ":" + verse;
+        curParse = bcv.parse(ref);
+        if (curParse.osis() == "") {
+          verse = 1;
+          chapter++;
+          ref = book + " " + chapter + ":" + verse;
+          if (bcv.parse(ref).osis() == "") {
+            return verses.map(function(v) { return bcv.parse(v).osis(); });
+	  }
+       }
+        verses.push(ref);
+        verse++;
+      }
+    case 'range':
+      start = passage.entities[0].passages[0].start;
+      end = passage.entities[0].passages[0].end;
+      endRef = end.b + " " + end.c + ":" + end.v;
+      book = start.b;
+      chapter = start.c;
+      verse = start.v || 1;
+      var verses = [];
+      while (true) {
+	ref = book + " " + chapter + ":" + verse;
+	curParse = bcv.parse(ref); 
+        if (ref == endRef) {
+	  verses.push(ref);
+          return verses.map(function(v) { return bcv.parse(v).osis(); });
+	}
+        if (curParse.osis() == "") {
+	  verse = 1;
+          chapter++;
+	  ref = book + " " + chapter + ":" + verse;
+	  if (chapter > end.c || bcv.parse(ref).osis() == "") {
+            return verses.map(function(v) { return bcv.parse(v).osis(); });
+	  }
+	}
+	verses.push(ref);
+        verse++;
+      }
+      return verses.map(function(v) { return bcv.parse(v).osis(); });
+    case 'sequence':
+      return passage.osis()
+                    .split(",")
+                    .map(function(p) { return getVerses(p); } )
+                    .reduce(function(a,b) { return a.concat(b); })
+                    .map(function(v) { return bcv.parse(v).osis(); });
+    default:
+      return [];
+  }
+}
+
+var worker = new Worker('application.js');
+
+worker.onmessage = function(oEvent) {
+  document.getElementById("output").remove();
+  newNode = document.createElement("div");
+  newNode.id = "output";
+  document.body.appendChild(newNode);
+
+  document.getElementById("output").innerHTML = oEvent.data; 
+  document.getElementById("progress").style.display = "none";
+  document.getElementById("submit").innerHTML = "Retrieve passages";
+  document.getElementById("submit").className = "button white";
+  document.getElementById("submit").onclick = function onclick(event) { javascript: parse(); };
+}
+
+function parse() {
+      text = document.getElementById("input").value;
+
+      passages = bcv.parse(text).osis(); 
+      versesJSON = {};
+      verses = getVerses(passages).forEach( function(v) { v = v.split("."); v[0] = osis2bible[v[0]]; versesJSON[v.join(".")] = 1; }); 
+      passagesJSON = getJSON(passages);
+
+
+      worker.postMessage({"passages": passages, "passagesJSON": passagesJSON, "versesJSON": versesJSON});
+      document.getElementById("progress").style.display = "";
+      document.getElementById("submit").innerHTML = "Retrieving passages...";
+      document.getElementById("submit").className = "button white disabled";
+      document.getElementById("submit").onclick = ""; 
+}
+
+function selectText(containerid) { 
+        if (document.selection) {
+            var range = document.body.createTextRange();
+            range.moveToElementText(document.getElementById(containerid));            
+	    range.select();        
+	} else if (window.getSelection) {            
+	    window.getSelection().removeAllRanges();            
+	    var range = document.createRange();            
+	    range.selectNode(document.getElementById(containerid));            
+	    window.getSelection().addRange(range);        
+	}    
+}
+
+function toggleVerses(translation) {
+      verseNums = document.getElementById(translation).getElementsByClassName('versenum');
+      Array.prototype.filter.call(verseNums, function(verseNum) { 
+        verseNum.style.display = (verseNum.style.display == '' ? 'none' : '');
+      });
+}
+
+function loadText() {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", "init.html");
+  xmlHttp.send(null);
+
+  xmlHttp.onreadystatechange = function() {
+    document.getElementById("output").remove();
+    newNode = document.createElement("div");
+    newNode.id = "output";
+    document.body.appendChild(newNode);
+
+    document.getElementById("output").innerHTML = xmlHttp.responseText;
+  }
+}
