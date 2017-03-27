33c64a40e170380fd4e9a8935f0192c5a4ab719e
Jonathan Yen <jonyen@jonyen-macbook-pro.local>
2017-03-26 22:47:50 -0700

    updated parser

--- assets/application2.php
+++ assets/application2.php
@@ -1,17 +1,23 @@
 <?php
-//$_GET['passages'] = "{%22John.3.16%22:[%22John%203%22],%22Ps.46.10%22:[%22Ps%2046%22],%221John.1.9-1John.1.10%22:[%221John%201%22]}";
-//$_GET['verses'] = "{%22JHN.3.16%22:1,%22PSA.46.10%22:1,%221JN.1.9%22:1,%221JN.1.10%22:1}";
+$_GET['passages'] = "{%22John.3.16%22:[%22John%203%22],%22Ps.46.10%22:[%22Ps%2046%22],%221John.1.9-1John.1.10%22:[%221John%201%22]}";
+$_GET['verses'] = "{%22JHN.3.16%22:1,%22PSA.46.10%22:1,%221JN.1.9%22:1,%221JN.1.10%22:1}";
+
+$bcv_books = ["Gen", "Exod", "Lev", "Num", "Deut", "Josh", "Judg", "Ruth", "1Sam", "2Sam", "1Kgs", "2Kgs", "1Chr", "2Chr", "Ezra", "Neh", "Esth", "Job", "Ps", "Prov", "Eccl", "Song", "Isa", "Jer", "Lam", "Ezek", "Dan", "Hos", "Joel", "Amos", "Obad", "Jonah", "Mic", "Nah", "Hab", "Zeph", "Hag", "Zech", "Mal", "Matt", "Mark", "Luke", "John", "Acts", "Rom", "1Cor", "2Cor", "Gal", "Eph", "Phil", "Col", "1Thess", "2Thess", "1Tim", "2Tim", "Titus", "Phlm", "Heb", "Jas", "1Pet", "2Pet", "1John", "2John", "3John", "Judge", "Rev"];
+
+$bible_com_books = ["GEN", "EXO", "LEV", "NUM", "DEU", "JOS", "JDG", "RUT", "1SA", "2SA", "1KI", "2KI", "1CH", "2CH", "EZR", "NEH", "EST", "JOB", "PSA", "PRO", "ECC", "SNG", "ISA", "JER", "LAM", "EZK", "DAN", "HOS", "JOL", "AMO", "OBA", "JON", "MIC", "NAM", "HAB", "ZEP", "HAG", "ZEC", "MAL", "MAT", "MRK", "LUK", "JHN", "ACT", "ROM", "1CO", "2CO", "GAL", "EPH", "PHP", "COL", "1TH", "2TH", "1TI", "2TI", "TIT", "PHM", "HEB", "JAS", "1PE", "2PE", "1JN", "2JN", "3JN", "JUD", "REV"];
 
 $passages = json_decode(urldecode($_GET['passages']));
 $verses = json_decode(urldecode($_GET['verses']), true);
 
-$translations = ["KRV" => "86", "KHSV" => "85", "JLB" => "83"]; // mappings for Korean & Khmer on Bible.com
+
+$translations = ["ESV" => "1"]; // mappings for Korean, Khmer, & Japanese on Bible.com
+//$translations = ["KRV" => "86", "KHSV" => "85", "JLB" => "83"]; // mappings for Korean, Khmer, & Japanese on Bible.com
 
 $khmer_nums = array("០","១","២","៣","៤","៥","៦","៧","៨","៩");
 
-$biblegateway_url="https://www.biblegateway.com/passage/";
+$biblegateway_url = "https://www.biblegateway.com/passage/";
 // Since Korean and Khmer are not available on Biblegateway, we use Bible.com
-$bible_com_url="https://www.bible.com/bible";
+$bible_com_url = "https://www.bible.com/bible";
 
 foreach(array_keys($translations) as $translation) {
   echo "<div class='block'>";
@@ -22,7 +28,6 @@ foreach(array_keys($translations) as $translation) {
   $bibleID = $translations[$translation];
   foreach($passages as $ref => $chapters) {
     foreach($chapters as $chapter) {
-//      echo "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n$$$$$$$$$$$$$$$$$$$$$$$$\n$$$$$$$$$$$$$$$$$$$$$$$$";
       $verseNums = curl_init();
       curl_setopt($verseNums, CURLOPT_URL, $biblegateway_url);
       curl_setopt($verseNums, CURLOPT_POSTFIELDS, "search=$ref&version=ESV");
@@ -37,7 +42,9 @@ foreach(array_keys($translations) as $translation) {
       }
 
       echo "<div class='passages'>";
-      $passage = rawurlencode($chapter);
+      $passage_array = explode(" ", $chapter);
+      $passage_array[0] = $bible_com_books[array_search($passage_array[0], $bcv_books)];
+      $passage = rawurlencode(join(".", $passage_array));
       $curl_handle = curl_init();
 
       curl_setopt($curl_handle, CURLOPT_URL, "$bible_com_url/$bibleID/$passage");
@@ -45,10 +52,14 @@ foreach(array_keys($translations) as $translation) {
 
       $result = curl_exec($curl_handle);
 
-//      echo $result;
+      echo $result;
+/*      echo "\n\n$$$$\n";
+      echo "$bible_com_url/$bibleID/$passage\n";
+      echo $result;
+      echo "----$$$$\n\n"; */
 
       //preg_match("/<article class='reader'.+?data-book-human='(.+?)'.+?data-chapter='(.+?)'.+?id='reader'>/", $result, $matches);
- //     echo "*************************\n*************************\n*************************";
+//      echo "*************************\n*************************\n*************************";
       preg_match("/<a class='book.+?>(.+?)<\/a>/", $result, $matches);
       $book = $matches[1];
 
@@ -58,13 +69,18 @@ foreach(array_keys($translations) as $translation) {
       echo  "<div style='text-align: center'><span class='fleuron'>d</span>  $book $verseNums  <span class='fleuron'>c</span></div>";
 
       //preg_match("/<!-- \/ Primary version content -->(.+?)<!-- \/ Secondary version content -->/s", $result, $matches);
-      preg_match("/<div class=\"version.+?>(.+?)<div class='copyright'>/s", $result, $matches);
+/*      preg_match("/<div class=\"version vid.+?>(.+?)/s", $result, $matches); */
+      preg_match("/<div class=\"version vid.+?>(.*)<div class=\"version-copyright/s", $result, $matches);
+      /* preg_match("/<div class=\"version.+?>(.+?)<div class='copyright'>/s", $result, $matches); */
       $content = $matches[1];
+      //echo $content;
+
       $dom = new DOMDocument();
 //      echo "---START----\n";
 //      print_r($content);
- //     echo "---END----\n";
+//      echo "---END----\n";
       $dom->loadHTML($content);
+      //$dom->loadHTML($result);
 
       $nodes = $dom->getElementsByTagName("span");
 
